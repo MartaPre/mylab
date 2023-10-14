502180c3f0d52336e7d1210982a36ad6be22004b
Koaxial <koaxial@localhost.localdomain>
2023-10-14 10:02:30 +0200

    Updates

---
+++ precache-manifest.1c178800db3f032baec7d6fa5092413e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d786d91f3e6ca5f0091e949f7a4aa53a",
+    "url": "/mylab/index.html"
+  },
+  {
+    "revision": "09bcde4a016694d8ee47",
+    "url": "/mylab/static/css/main.0132acbb.chunk.css"
+  },
+  {
+    "revision": "17336c7bb18402d0a27b",
+    "url": "/mylab/static/js/2.9e20096d.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/mylab/static/js/2.9e20096d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f97354ec5b81692ee58c",
+    "url": "/mylab/static/js/3.557593b2.chunk.js"
+  },
+  {
+    "revision": "09bcde4a016694d8ee47",
+    "url": "/mylab/static/js/main.ab3daaff.chunk.js"
+  },
+  {
+    "revision": "b3544918603b05a76850",
+    "url": "/mylab/static/js/runtime-main.338a5d16.js"
+  },
+  {
+    "revision": "f8cd8e48e6b34e990b43192fec829d94",
+    "url": "/mylab/static/media/lain-icon.f8cd8e48.jpg"
+  },
+  {
+    "revision": "0748b438aa587583d95accc352247a41",
+    "url": "/mylab/static/media/lainicon1.0748b438.jpg"
+  },
+  {
+    "revision": "415bc5676a05e513400aecccdb481ee1",
+    "url": "/mylab/static/media/lainicon2.415bc567.jpg"
+  },
+  {
+    "revision": "7197f923a32b627e8d7b7ea390cb51ef",
+    "url": "/mylab/static/media/lainicon3.7197f923.jpg"
+  },
+  {
+    "revision": "848d47bfb98b5012c1446116f6741873",
+    "url": "/mylab/static/media/lainicon4.848d47bf.jpg"
+  }
+]);
