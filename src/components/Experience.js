07b5362ea52b06a2b930570e47b1e1b63149d6ca
Marta Pretel <marta.ruidosordo@gmail.com>
2024-03-09 15:56:18 +0100

    add experience field

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -4,9 +4,21 @@ function Experience() {
   return (
     <section className="cv-section">
       <h2>Experience</h2>
+
+      <h3>Frontend developer - Capgemini, Client Openbank</h3>
+      <p className="date-info">Valencia, Valencia - 11/2023 to Current</p>
+      <p>
+        Member of Openbank Cobranded WEB team.
+        <br />
+        Environment: Amazon Web Services (AWS)
+        <br />
+        Technologies: TypeScript, React.
+        <br />
+        Project version controller with GIT, QuickSilver, Bamboo & Bitbucket.
+      </p>
       
       <h3>Frontend developer - Sopra Steria, Client BBVA</h3>
-      <p className="date-info">Valencia, Valencia - 11/2021 to Current</p>
+      <p className="date-info">Valencia, Valencia - 11/2021 to 11/2023</p>
       <p>
         Member of the BBVA C-FIT team in the Corporate & Investment Banking division. Maintenance and development of new functionalities 
         of a web application in charge of carrying out prices and calibrations of underlying assets in real time, integrated with Bloomberg, 
